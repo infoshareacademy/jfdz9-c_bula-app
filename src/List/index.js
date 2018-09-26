0a7720d5d2a3c48ac36a1f46c3e6168a6ebe8865
tklenk <tatiana.klenk@gmail.com>
2018-09-27 00:19:37 +0200

    fetching test data from json

--- src/List/index.js
+++ src/List/index.js
@@ -1,10 +1,43 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 
 class List extends Component {
-    render() {
-        return ( <h2>a tu listy</h2>
 
+    state = {
+        shops: []
+    };
+
+   componentDidMount() {
+       fetch('/data/shops.json')
+           .then(response => response.json())
+           .then(shops => {
+               this.setState({
+                   shops: shops,
+               })
+           })
+};
+
+
+    render() {
+        return (
+            <Fragment>
+                <h2>a tu są listy jak na razie:</h2>
+
+                    {this.state.shops.map(shop => {
+                        return <div key={shop.id}>
+                            <h4>{shop.name}</h4>
+                            <p>{shop.description}</p>
+                            <p>{shop.address.postalCode} {shop.address.district} {shop.address.street}</p>
+                            <p>{`Pn-Pt ${shop.openingHours.weekday_open} - ${shop.openingHours.weekday_close}`} </p>
+                            <p>{`Sb ${shop.openingHours.saturday_open} - ${shop.openingHours.saturday_close}`} </p>
+                            <p>{`Nd ${shop.openingHours.sunday_open} - ${shop.openingHours.sunday_close}`} </p>
+                        </div>
+                    })}
+
+                <div>
+
+                </div>
+            </Fragment>
     );
     }
 }
