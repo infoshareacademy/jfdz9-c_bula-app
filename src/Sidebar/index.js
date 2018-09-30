a2de90c8d1144a45159681bcb9db3d053cf5923a
Paweł Nowicki <nowicki.p@op.pl>
2018-09-30 18:09:15 +0200

    refactoring

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,72 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import CheckboxesGroup from "./CheckboxesGroup";
+import ControlledOpenSelect from "./ControlledOpenSelect";
 
-const styles = theme => ({
-    root: {
-        display: 'flex',
-    },
-    formControl: {
-        margin: theme.spacing.unit * 3,
-    },
-});
+export default Sidebar;
 
-class CheckboxesGroup extends React.Component {
-    state = {
-        piekarnie: true,
-        warzywniaki: false,
-        monopolowe: false,
-    };
-
-    handleChange = name => event => {
-        this.setState({ [name]: event.target.checked });
-    };
-
-    render() {
-        const { classes } = this.props;
-        const { piekarnie, warzywniaki, monopolowe } = this.state;
-
-        return (
-            <div className={classes.root}>
-                <FormControl component="fieldset" className={classes.formControl}>
-                    <FormLabel component="legend"><h2>Wybierz kategorie sklepów:</h2></FormLabel>
-                    <FormGroup>
-                        <FormControlLabel
-                            control={
-                                <Checkbox checked={piekarnie} onChange={this.handleChange('piekarnie')} value="piekarnie" />
-                            }
-                            label="Piekarnie"
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox checked={warzywniaki} onChange={this.handleChange('warzywniaki')} value="warzywniaki" />
-                            }
-                            label="Warzywniaki"
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    checked={monopolowe}
-                                    onChange={this.handleChange('monopolowe')}
-                                    value="monopolowe"
-                                />
-                            }
-                            label="Monopolowe"
-                        />
-                    </FormGroup>
-                </FormControl>
-            </div>
-        );
-    }
-}
-
-CheckboxesGroup.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(CheckboxesGroup);
+export {default as Sidebar} from './Sidebar';
