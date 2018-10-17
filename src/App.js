d5899b6cca69ae85a6a9bf07f94046c154ac236a
Marta Pienkowska <marta.pienkowska@gmail.com>
2018-10-17 10:38:48 +0200

    Nav Grid is back

--- src/App.js
+++ src/App.js
@@ -40,6 +40,9 @@ class App extends Component {
                     <div>
                         <Grid container spacing={0}>
                             <Grid item xs={12}>
+                                <Nav/>
+                            </Grid>
+                            <Grid item xs={12}>
                                 <Route path="/dashboard" component={Dashboard}/>
                                 <Route path="/list" component={List}/>
                                 <Route path="/home" component={Home}/>
