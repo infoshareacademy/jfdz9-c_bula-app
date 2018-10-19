39abaf30594b6b281ed4e9c04f3c3608ce9f13ad
tklenk <tatiana.klenk@gmail.com>
2018-10-19 16:04:34 +0200

    added icons in navBar, change backgr

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -4,6 +4,10 @@ import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
 import Button from "@material-ui/core/es/Button/Button";
 import {Link} from 'react-router-dom';
 import logo from './logo.png';
+import signin from './signin.svg';
+import signup from './signup.svg';
+import search from './search.svg';
+
 import './nav.css';
 // import SignIn from './SignIn/index.js';
 // import SignUp from './SignUp';
@@ -20,13 +24,19 @@ function Nav() {
 
                     <div className="navButtonAll">
                         <Button className="navButton">
-                            <Link to="/home">Wyszukaj</Link>
+                            <Link to="/home"><img src={search} alt=""/>
+                                <div className="navButtonIcon">Wyszukaj</div>
+                            </Link>
                         </Button>
                         <Button className="navButton">
-                            <Link to="/signIn">Sign In</Link>
+                            <Link to="/signIn"><img src={signin} alt=""/>
+                                <div className="navButtonIcon">Zaloguj</div>
+                            </Link>
                         </Button>
                         <Button className="navButton">
-                            <Link to="/signUp">Sign Up</Link>
+                            <Link to="/signUp"><img src={signup} alt=""/>
+                                <div className="navButtonIcon">Zarejestruj</div>
+                            </Link>
                         </Button>
                     </div>
                 </Toolbar>
